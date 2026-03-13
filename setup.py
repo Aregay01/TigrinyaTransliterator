b2c96a96ff5e12b817efd2bd1e35c4b734438650
Aregay Asmelash <aregaya1@gmail.com>
2026-03-12 23:21:40 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tigrinya_transliterator",
+    version="0.1.0",
+    py_modules=["ti_transliterator"],  # because your file is ti_transliterator.py
+    author="Aregay",
+    description="Tigrinya Latin to Ge'ez transliterator",
+    license="MIT",
+    url="https://github.com/Aregay01/TigrinyaTransliterator",
+)
